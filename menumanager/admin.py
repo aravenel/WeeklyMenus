4494d52ea0a75329ea691f698d4a112f36e8e67b
Alex Ravenel <ravenel@gmail.com>
2012-08-05 21:01:46 -0400

    Enabled admin

---
+++ menumanager/admin.py
@@ -0,0 +1,6 @@
+from menumanager.models import WeeklyMenu, Menu, MenuItem
+from django.contrib import admin
+
+admin.site.register(WeeklyMenu)
+admin.site.register(Menu)
+admin.site.register(MenuItem)
---
+++ recipemanager/admin.py
@@ -0,0 +1,4 @@
+from recipemanager.models import Recipe
+from django.contrib import admin
+
+admin.site.register(Recipe)
