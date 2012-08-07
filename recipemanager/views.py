8c255ad5fae4fa689bcbb3ae863012d9a41b7523
Alex Ravenel <ravenel@gmail.com>
2012-08-06 22:18:07 -0400

    Minor view updates.

--- recipemanager/views.py
+++ recipemanager/views.py
@@ -2,6 +2,18 @@ from recipemanager.models import Recipe, RecipeForm
 from django.shortcuts import render_to_response
 # Create your views here.
 
+def index(request):
+
+    add_form = RecipeForm()
+    recent_recipes = Recipe.objects.orderby('last_made')[:5]
+    popular_recipes = Recipe.objects.orderby('made_count')[:5]
+
+    return render_to_response('recipes.html',{
+        'add_form': add_form,
+        'recent_recipes': recent_recipes,
+        'popular_recipes': popular_recipes,
+        })
+
 def add(request):
     if request.method == 'POST':
         pass
