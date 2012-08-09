01c42a4dcd105a988978c638a086168fe882274a
Alex Ravenel <ravenel@gmail.com>
2012-08-09 13:26:11 -0400

    Start of menumanager app.

--- menumanager/models.py
+++ menumanager/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.forms import ModelForm
+from crispy_forms.helper import FormHelper
+from crispy_forms.bootstrap import FormActions
+from crispy_forms.layout import Submit, Layout, Field
 
 # Create your models here.
 class WeeklyMenu(models.Model):
@@ -8,6 +12,27 @@ class WeeklyMenu(models.Model):
 
     def __unicode__(self):
         return "Weekly Menu %s to %s" % (self.start_date, self.end_date)
+
+class WeeklyMenuForm(ModelForm):
+    #Form to add a new weekly menu
+    class Meta:
+        model = WeeklyMenu
+
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-WeeklyMenuForm'
+        self.helper.form_class = 'well form-inline'
+        self.helper.form_method = 'post'
+        self.helper.form_action = ''
+        self.helper.layout = Layout(
+                Field('start_date', css_class='datepicker'),
+                Field('end_date', css_class='datepicker'),
+                FormActions(
+                    Submit('submit', 'Add Menu', css_class='btn-primary'),
+                    ),
+                )
+
+        super(WeeklyMenuForm, self).__init__(*args, **kwargs)
 
 class Menu(models.Model):
     #Individual menu--breakfast, lunch, or dinner
--- menumanager/views.py
+++ menumanager/views.py
@@ -1,8 +1,34 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
+from menumanager.models import WeeklyMenu, WeeklyMenuForm
+from django.template import RequestContext
+import datetime
 
 # Create your views here.
 def index(request):
-    return render_to_response('index.html')
+    if request.method == 'POST':
+        weekly_menu_form = WeeklyMenuForm(request.POST)
+        if weekly_menu_form.is_valid():
+            weekly_menu_form.save()
+            return redirect('/menus')
+    else:
+        weekly_menu_form = WeeklyMenuForm()
+    
+    all_menus = WeeklyMenu.objects.all()
+    today = datetime.date.today()
+    current_menu = None
+    for menu in all_menus:
+        if today >= menu.start_date and today <= menu.end_date:
+            current_menu = menu
+            break
+
+    return render_to_response(
+            'weekly_menus.html',
+            {
+                'weekly_menu_form': weekly_menu_form,
+                'current_menu': current_menu,
+            },
+            context_instance=RequestContext(request)
+            )
 
 def add(request):
     pass
