2b4ce245271dae651b458829ab5cac1355fbda97
Alex Ravenel <ravenel@gmail.com>
2012-08-10 12:14:56 -0400

    Can now add recipes to menu

--- menumanager/urls.py
+++ menumanager/urls.py
@@ -7,8 +7,9 @@ urlpatterns = patterns('',
     #url(r'^recipes/', include('recipes.urls')),
     #url(r'^menus/', include('menus.urls')),
     url(r'^$', 'menumanager.views.index'),
-    url(r'^add/$', 'menumanager.views.add'),
     url(r'^(?P<weeklymenu_id>\d)/$', 'menumanager.views.weekly_edit'),
-    url(r'^(?P<weeklymenu_id>\d)/(?P<unix_date>\d)/(?P<menu_type>\d)/$',
-        'menumanager.views.menu_edit')
+    url(r'(?P<weeklymenu_id>\d+)/(?P<menu_date>\d+)/(?P<menu_type>\d+)/$',
+        'menumanager.views.menu_edit'),
+    url(r'(?P<weeklymenu_id>\d+)/(?P<menu_date>\d+)/(?P<menu_type>\d+)/add/(?P<recipe_id>\d+)/$',
+        'menumanager.views.recipe_add')
 )
--- menumanager/views.py
+++ menumanager/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render_to_response, redirect
-from menumanager.models import WeeklyMenu, WeeklyMenuForm
+from django.shortcuts import render_to_response, redirect, get_object_or_404
+from menumanager.models import WeeklyMenu, WeeklyMenuForm, MenuItem
+from recipemanager.models import Recipe
 from django.template import RequestContext
 import datetime
 
@@ -38,11 +39,31 @@ def index(request):
             context_instance=RequestContext(request)
             )
 
-def add(request):
-    pass
+def menu_edit(request, weeklymenu_id, menu_date, menu_type):
+    dt = datetime.datetime.strptime(menu_date, "%Y%m%d").date()
+    current_recipes = MenuItem.objects.filter(menu=weeklymenu_id, menu_date=dt,
+            menu_type=menu_type)
+    recent_recipes = Recipe.objects.order_by('-last_made')[:5]
+    popular_recipes = Recipe.objects.order_by('made_count')[:5]
 
-def menu_edit(request):
-    pass
+    return render_to_response(
+            'menu_items.html',
+            {
+                'current_recipes': current_recipes,
+                'recent_recipes': recent_recipes,
+                'popular_recipes': popular_recipes,
+            },
+            context_instance=RequestContext(request)
+            )
+
+def recipe_add(request, weeklymenu_id, menu_date, menu_type, recipe_id):
+    menu = get_object_or_404(WeeklyMenu, pk=weeklymenu_id)
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    dt = datetime.datetime.strptime(menu_date, "%Y%m%d").date()
+    mi = MenuItem(menu=menu, menu_date=dt, menu_type=menu_type, recipe=recipe)
+    mi.save()
+    next = request.GET.get('next')
+    return redirect(next)
 
 def weekly_edit(request):
     pass
