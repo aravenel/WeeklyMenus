20471f861389051d61ab1eaeacb0db5ef49db31e
Alex Ravenel <ravenel@gmail.com>
2012-08-24 14:38:25 -0400

    User login/logout

--- WeeklyMenus/urls.py
+++ WeeklyMenus/urls.py
@@ -17,6 +17,9 @@ urlpatterns = patterns('',
     (r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #Login form
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login'),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
