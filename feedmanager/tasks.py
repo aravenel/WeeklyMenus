5ff50350a4dacfeb6628ccb657ee420ca0e66cec
Alex Ravenel <ravenel@gmail.com>
2012-08-25 15:12:53 -0400

    Minor task bug fixes and cleanup

--- feedmanager/tasks.py
+++ feedmanager/tasks.py
@@ -12,30 +12,16 @@ def update_feed(feed, request):
 @task
 def update_feed_pinboard(feed, user):
     """Get pinboard feed of recipes and kick of subtasks to process them"""
-    #feed_time = feed.updated.strftime('%Y-%m-%dT%H:%M:%SZ')
-    #feed_url = 'https://api.pinboard.in/v1/posts/all?auth_token=%s&tag=%s&format=json&fromdt=%s' % (
-            #feed.feed_apikey, feed.feed_tag_key, feed_time)
-    feed_url = 'https://api.pinboard.in/v1/posts/all?auth_token=%s&tag=%s&format=json' % (
-            feed.feed_apikey, feed.feed_tag_key)
-    
+    feed_time = feed.updated.strftime('%Y-%m-%dT%H:%M:%SZ')
+    feed_url = 'https://api.pinboard.in/v1/posts/all?auth_token=%s&tag=%s&format=json&fromdt=%s' % (
+            feed.feed_apikey, feed.feed_tag_key, feed_time)
+
+    #Hit API
     r = requests.get(feed_url)
+
     if r.status_code == 200:
         recipes = json.loads(r.content)
         print "%s NEW RECIPES FROM PINBOARD" % len(recipes)
-        #List to hold all subtasks for celery task group
-        #recipe_tasks = []
-        #for recipe in recipes:
-            ##Get the required information
-            #url = recipe['href']
-            #title = recipe['description']
-            #owner = user
-            #source = 'pinboard'
-            #tags = recipe['tags']
-
-            ##Instantiate the task and add it to list for group
-            #recipe_task = add_recipe.s(url, title, owner,
-                    #source, tags)
-            #recipe_tasks.append(recipe_task)
 
         #Mark completion time so that we know when feed was updated
         #NOTE: This leaves small gap of time b/t calling of task and finishing
@@ -43,12 +29,13 @@ def update_feed_pinboard(feed, user):
         feed.updated = datetime.datetime.utcnow()
         feed.save()
 
-        #Call group of subtasks
-        job = group(
-                [add_recipe.s(recipe['href'], recipe['description'],
-                    user, 'pinboard', recipe['hash'], recipe['tags'])
-                for recipe in recipes]
-                )
-        #job = group(recipe_tasks)
-        print "STARTING NEW RECIPE SUBTASKS"
-        result = job.apply_async()
+        if len(recipes) > 0:
+            #Call group of subtasks
+            job = group(
+                    [add_recipe.s(recipe['href'], recipe['description'],
+                        user, 'pinboard', recipe['hash'], recipe['tags'])
+                    for recipe in recipes]
+                    )
+
+            print "STARTING NEW RECIPE SUBTASKS"
+            result = job.apply_async()
