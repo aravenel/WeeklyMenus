c80c1e4683d19196b8ba1e27c90d6424d18df2ec
Alex Ravenel <ravenel@gmail.com>
2012-09-19 17:51:12 -0400

    Tweaks to fabfile.

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,7 @@ from fabric.api import *
 #from fabric.operations import *
 import os
 from sys import exit
+from socket import gethostname
 
 CONFIG = {
         'dev': {
@@ -13,20 +14,25 @@ CONFIG = {
             #Location where the code is stored
             'code_dir': '/home/ravenel/apps/menus-staging/WeeklyMenus',
             #Location of python executable--for virtualenv
-            'python': os.path.join(CONFIG['staging']['code_dir'], 
-                'venv/bin/python'),
+            'python': '/home/ravenel/apps/menus-staging/WeeklyMenus/venv/bin/python',
             #gunicorn server name (in supervisord)
             'gunicorn_name': 'menus-staging',
             #celeryd server name (in supervisord)
             'celeryd_name': 'menus-staging-celeryd',
             #Hosts to use
             'hosts': [
-                'http://menus-dev.alexravenel.com',
+                #'http://menus-dev.alexravenel.com',
+                'www.alexravenel.com'
                 ],
             },
         'prod': {
             },
         }
+
+key_locations = {
+        'usnycaravenel1': r'C:\Users\aravenel\pub.ppk',
+        }
+
 
 def staging():
     env.hosts = CONFIG['staging']['hosts']
@@ -36,6 +42,8 @@ def staging():
     env.python = CONFIG['staging']['python']
     env.gunicorn_name = CONFIG['staging']['gunicorn_name']
     env.celeryd_name = CONFIG['staging']['celeryd_name']
+    env.user = 'ravenel'
+    env.key_filename = [key_locations[gethostname()]]
 
 def prod():
     print "\n\nWARNING! You are about to deploy to production!\n\n"
@@ -57,6 +65,7 @@ def deploy():
         #Checkout new code
         #sudo('git pull')
         #sudo('git checkout %s' % env.repo)
+        print "Checking out code...",
         run('git pull')
         run('git checkout %s' % env.repo)
 
@@ -66,26 +75,39 @@ def deploy():
             put(settings_file, 'settings')
         else:
             print "Settings file %s does not exist. Cannot copy to host." % settings_file
+        print "Done."
 
         #Sync DB
         #sudo('python manage.py syncdb --settings=settings.%s' % env.environment)
+        print "Syncing DB..."
         run('%s manage.py syncdb --settings=settings.%s' % (env.python, env.environment))
+        print "Done."
 
         #Run South migrations
         #sudo('python manage.py migrate --all --settings=settings.%s' % env.environment)
+        print "Running South migrations..."
         run('%s manage.py migrate --all --settings=settings.%s' % (env.python, env.environment))
+        print "Done."
 
         #Collect static
+        print "Collecting static files..."
         run('%s manage.py collectstatic --settings=settings.%s' % (env.python, env.environment))
+        print "Done."
 
         #Restart redis
         #sudo('service redis-server restart')
 
         #Restart celery workers
-        sudo('supervisorctl restart %s' % env.celeryd_name)
+        #print "Restarting celery workers...."
+        #sudo('supervisorctl restart %s' % env.celeryd_name)
+        #print "Done."
 
         #Restart gunicorn server
+        print "Restarting gunicorn server..."
         sudo('supervisorctl restart %s' % env.gunicorn_name)
+        print "Done."
 
         #Restart nginx server
+        print "Restarting nginx..."
         sudo('service nginx restart')
+        print "Done."
