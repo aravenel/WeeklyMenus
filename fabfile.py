b6480a82d70a463de27eb9a1e1fd7ac806ae895f
Alex Ravenel <ravenel@gmail.com>
2013-04-01 16:32:09 -0400

    First sorta working vagrant version

--- fabfile.py
+++ fabfile.py
@@ -23,6 +23,21 @@ CONFIG = {
                 'www.alexravenel.com'
                 ],
             },
+        'vagrant': {
+            #Name of the branch to use
+            'repo': 'staging',
+            #Location where the code is stored
+            'code_dir': '~/apps/menus-staging/WeeklyMenus',
+            #Location of python executable--for virtualenv
+            'python': '~/apps/menus-staging/WeeklyMenus/venv/bin/python',
+            #Group containing programs in supervisord.conf
+            'supervisord_group': 'menus-staging',
+            #Hosts to use
+            'hosts': [
+                #'http://menus-dev.alexravenel.com',
+                'www.alexravenel.com'
+                ],
+            },
         'prod': {
             },
         }
@@ -45,14 +60,20 @@ def staging():
     env.key_filename = key_locations[gethostname()]
 
 def vagrant():
-    env.hosts = ['127.0.0.1:8080']
-    env.users = 'vagrant'
+    env.hosts = ['127.0.0.1:2222']
+    env.code_dir = CONFIG['vagrant']['code_dir']
+    env.user = 'vagrant'
     env.repo = CONFIG['staging']['repo']
-    env.environment = 'staging'
+    env.environment = 'vagrant'
     env.merges_from = 'develop'
     env.python = CONFIG['staging']['python']
     env.supervisord_group = CONFIG['staging']['supervisord_group']
-    env.key_filename = key_locations[gethostname()]
+    ssh_keyfile = local('vagrant ssh-config | grep IdentityFile', capture=True)
+    if os.name == 'nt':
+        env.key_filename = ssh_keyfile.split()[1].replace(r'/', '\\').replace('\"', '')
+    else:
+        env.key_filename = ssh_keyfile.split()[1]
+
 
 def prod():
     print "\n\nWARNING! You are about to deploy to production!\n\n"
@@ -79,13 +100,23 @@ def push():
 
 def provision():
     #install basic background
-    sudo('apt-get update')
-    sudo('apt-get -y upgrade')
-    sudo('apt-get -y install python python-pip nginx redis-server')
+    sudo('apt-get -qq update')
+    sudo('apt-get -y -qq upgrade')
+    sudo('apt-get -y -qq install python python-pip nginx redis-server mysql-server python-mysqldb')
     sudo('update-rc.d nginx defaults')
     #install python packages
     sudo('pip install supervisor gunicorn')
+    #make directories
+    sudo('mkdir -p /srv/www/menus-dev')
+    run('mkdir -p ~/apps/menus-staging')
+    sudo('ln -s /vagrant /srv/www/menus-dev/http')
     #deploy config files
+    #nginx config
+    if env.environment == 'vagrant':
+        put('config/nginx.conf', '/etc/nginx/nginx.conf', use_sudo=True)
+        put('config/nginx-enabled.conf', '/etx/nginx/sites-available/', use_sudo=True)
+    elif env.environment == 'staging':
+        pass
 
 def deploy():
     with cd(env.code_dir):
---
+++ settings/vagrant.py
@@ -0,0 +1,58 @@
+from common import *
+from sys import exit
+
+try:
+    from passwords_staging import *
+except ImportError:
+    print "Cannot import passwords_staging file."
+    exit()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': DB_NAME,
+        'USER': DB_USER,                      # Not used with sqlite3.
+        'PASSWORD': DB_PASSWORD,                  # Not used with sqlite3.
+        'HOST': DB_HOST,                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': DB_PORT,                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.sessions',
+    'crispy_forms',
+    'ajax_select',
+    'taggit',
+    'taggit_autosuggest',
+    'south',
+    'menumanager',
+    'recipemanager',
+    'feedmanager',
+    'djcelery',
+    'taggit_templatetags',
+    #'debug_toolbar',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+DEBUG = True
+
+STATIC_ROOT = '/home/ravenel/apps/menus-staging/static_files'
+
+BROKER_URL = 'redis://localhost:6379/0'
