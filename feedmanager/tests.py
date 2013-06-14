5e04c98cc893caf085b047e2b9e8ae07eec0a0ed
Alex Ravenel <ravenel@gmail.com>
2013-06-14 16:29:21 -0400

    Started tests (barely)

--- feedmanager/tests.py
+++ feedmanager/tests.py
@@ -6,11 +6,30 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.test.utils import setup_test_environment
+from models import Recipe
+import datetime
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+def create_recipe():
+	pass
+
+class RecipeTestCase(TestCase):
+	# def setUp(self):
+	# 	Recipe.objects.create(
+	# 		url = 'http://www.google.com',
+	# 	    title = 'test title'
+	# 	    added = datetime.datetime.now()
+	# 	    made_count = datetime.datetime.now()
+	# 	    comments = 'Test comments'
+	# 	    owner = None
+	# 	    #Used for hash of recipe title (e.g. from Pinboard)--used for dupe detection
+	# 	    hash = 'test hash'
+	# 	    source = 'Test source'
+	# 	    rating = 5
+	# 	    content = 'Test content'
+	# 	    image = None
+	# 		)
+	
+	def test_recipe_view_all(self):
+		pass
