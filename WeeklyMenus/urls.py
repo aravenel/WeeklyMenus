0f2da952a3ccf6da3abd71fd6f8e2db9184411cd
Alex Ravenel <ravenel@gmail.com>
2013-11-10 12:11:46 -0500

    Logout template was missing

--- WeeklyMenus/urls.py
+++ WeeklyMenus/urls.py
@@ -22,8 +22,8 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     #Login form
-    #(r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    #(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login'),
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login'),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
